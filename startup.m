2bfb09853cbb4884eff54c1bb5cc5f68c50ff272
Mattia Fussi <38140169+mfussi66@users.noreply.github.com>
2023-07-05 10:19:18 +0200

    Add files via upload

---
+++ startup.m
@@ -0,0 +1,5 @@
+set(groot,'defaultLineLineWidth',1.3);
+set(groot,'defaultAxesFontSize',12);
+set(groot,'defaultAxesMinorGridAlpha', 1);
+set(groot,'defaultAxesXMinorGrid','on','defaultAxesXMinorGridMode','manual');
+set(groot,'defaultAxesYMinorGrid','on','defaultAxesYMinorGridMode','manual');
